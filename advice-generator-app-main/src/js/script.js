c4920922dd8960979a1dea4d3c4291f520853f0a
06Augusto-ziuL <132853357+06Augusto-ziuL@users.noreply.github.com>
2024-08-31 23:08:42 -0300

    New challenge

---
+++ advice-generator-app-main/src/js/script.js
@@ -0,0 +1,27 @@
+const adviceTitleId = document.querySelector('#random-advice')
+const textSpace = document.querySelector('#advice-text')
+const ChangeAdvice = document.querySelector('#change-advice-randomly')
+const apiUrl = "https://api.adviceslip.com/advice"
+
+function showAdvice(){
+    try {
+        fetch(apiUrl)
+        .then((res) => res.json())
+        .then((adviceData) => {
+            console.log(adviceData)
+            adviceTitleId.innerHTML = adviceData.slip.id
+            textSpace.innerHTML = `"${adviceData.slip.advice}"`
+        })
+        .finally(() => {
+            ChangeAdvice.style.animation = ''
+        })
+    } catch(error){
+        console.log(error)
+    } 
+}
+showAdvice()
+
+ChangeAdvice.addEventListener('click', () => {
+    ChangeAdvice.style.animation = 'spin 1s infinite'
+    showAdvice()  
+})
